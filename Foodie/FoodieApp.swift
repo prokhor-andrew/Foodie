90f226b9b53983e975836df1262cc9c007e1dd85
prokhor-andrew <prokhor.andrew@gmail.com>
2023-07-15 10:39:37 -0400

    Initial Commit

---
+++ Foodie/FoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by Andriy Prokhorenko on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
